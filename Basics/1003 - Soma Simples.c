1baca060ed6f6e686ae86ff9a9d23a701eab0b71
Everton Cardoso Acchetta <eve.023@hotmail.com>
2018-02-28 15:42:11 -0300

    Create 1003 - Soma Simples.c

---
+++ Basics/1003 - Soma Simples.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+ {
+    int A, B, SOMA;
+
+    scanf("%d %d",&A,&B);
+
+	SOMA=A+B;
+
+    printf("SOMA = %d\n",SOMA);
+
+	system("PAUSE");
+    return 0;
+}
