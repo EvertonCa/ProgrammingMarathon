bb3b2997ca11fd8f2b702818446ae51ecda3f9dc
Everton Cardoso Acchetta <eve.023@hotmail.com>
2018-02-28 16:14:12 -0300

    Create 1005 - Média 1.c

---
+++ Basics/1005 - Média 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+ {
+    double A, B, media;
+
+    scanf("%lf %lf",&A,&B);
+
+    media=((A*3.5)+(B*7.5))/11;
+    
+    printf("MEDIA = %0.5lf\n",media);
+
+	system("PAUSE");
+    return 0;
+}
