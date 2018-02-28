270addeff96df30d806a6f63508ee51f7fd0378c
Everton Cardoso Acchetta <eve.023@hotmail.com>
2018-02-28 16:13:48 -0300

    Create 1004 - Produto Simples.c

---
+++ Basics/1004 - Produto Simples.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+ {
+    int X, Y, PROD;
+
+    scanf("%d %d",&Y,&X);
+
+	PROD=Y*X;
+
+    printf("PROD = %d\n",PROD);
+
+	system("PAUSE");
+    return 0;
+}
